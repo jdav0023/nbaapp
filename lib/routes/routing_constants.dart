e7edf444dff1bf731ffa4ddf5ef60c3142033ea4
Jade Davis <jdav0023@student.monash.edu>
2022-12-12 18:41:54 +1100

    Removing unneeded files, dirs from Git index

--- lib/routes/routing_constants.dart
+++
@@ -1,7 +0,0 @@
-// ignore_for_file: constant_identifier_names
-
-const String LoginViewRoute = '/';
-const String HomeViewRoute = 'home';
-const String TeamsViewRoute = "teams";
-const String PlayersViewRoute = "players";
-const String GamesViewRoute = "games";
